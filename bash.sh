aaedf85522b0e57ad9c0941a0ab2f03d9e537d8b
sumanto-23 <64468496+sumanto-23@users.noreply.github.com>
2022-10-05 14:12:05 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,9 @@
 echo "Hello World!"
 ls
 kubectl create namespace crossplane-system
-kubectl config current-context --namespace crossplane-system
+helm repo add crossplane-stable https://charts.crossplane.io/stable
+helm repo update
+helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
+helm list -n crossplane-system
+kubectl get all -n crossplane-system
+curl -sL https://raw.githubusercontent.com/crossplane/crossplane/master/install.sh | sh
